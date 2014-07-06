7d17a021a657061d739da9eb70bc3e9da34817e7
craigmingtaozhang <mail2mingtao@gmail.com>
2014-07-06 11:22:49 -0400

    recursion -> map -> DP

--- src/main/java/org/mingtaoz/leetcode/string/DistinctSubsequences.java
+++ src/main/java/org/mingtaoz/leetcode/string/DistinctSubsequences.java
@@ -21,9 +21,21 @@ public class DistinctSubsequences {
 	 * @return
 	 */
 	public int numDistinct(String S, String T) {
-		
-		
-		
-		return 0;
+		int s = S.length(), t = T.length();
+		if (s == 0) return 0;
+		int[][] table = new int[s + 1][t + 1]; 
+		for (int i = 0; i < s + 1; i++) {
+			table[i][0] = 1;
+		}
+
+		for (int i = 1; i < s + 1; i++) {
+			for (int j = 1; j < t + 1; j++) {
+				// DEF: table[i][j] = numDistinct(S.sub(0, i), T.sub(0, j))
+				// REC: table[i][j] = if equals, plus left-up corner, always take previous number which matches T.sub(0, j)
+				table[i][j] = (S.charAt(i - 1) == T.charAt(j - 1) ? table[i - 1][j - 1] : 0) + table[i - 1][j];
+			}
+		}
+
+		return table[s][t];
 	}
 }
