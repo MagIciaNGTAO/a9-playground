80d28a7618578735bcafae1873d5bb89e1eb671e
craigmingtaozhang <mail2mingtao@gmail.com>
2014-08-03 19:25:55 -0400

    First Truth test.

--- src/test/java/org/mingtaoz/leetcode/set/SubsetsTest.java
+++ src/test/java/org/mingtaoz/leetcode/set/SubsetsTest.java
@@ -1,7 +1,17 @@
 package org.mingtaoz.leetcode.set;
 
+import static org.junit.Assert.*;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import com.google.common.truth.Expect;
+
+import static org.truth0.Truth.ASSERT;
 
 public class SubsetsTest {
 
@@ -13,7 +23,18 @@ public class SubsetsTest {
 	}
 
 	@Test
-	public void testSubsets1() {
-		System.out.println(sut.subsetsWithDup(new int[] { 1, 1 }));
+	public void testSubsetsWithDup1() {
+		List<Integer> l1 = new LinkedList<>();
+		List<Integer> l2 = new LinkedList<>();
+		l2.add(1);
+		List<Integer> l3 = new LinkedList<>();
+		l3.add(1);
+		l3.add(1);
+		List<List<Integer>> assertion = new LinkedList<>();
+		assertion.add(l1);
+		assertion.add(l2);
+		assertion.add(l3);
+		ASSERT.that(sut.subsetsWithDup(new int[] { 1, 1 })).containsSequence(
+				assertion);
 	}
 }
