feb9dcde3da327e65bc09714a6ab2e93e3cb02f2
craigmingtaozhang <mail2mingtao@gmail.com>
2014-08-17 23:14:36 -0400

    small prob

---
+++ src/main/java/org/mingtaoz/leetcode/matrix/SetMatrixZeroes.java
@@ -0,0 +1,24 @@
+package org.mingtaoz.leetcode.matrix;
+
+public class SetMatrixZeroes {
+	public void setZeroes(int[][] matrix) {
+		// TODO check?
+		int[] row = new int[matrix.length], column = new int[matrix[0].length];
+		for (int i = 0; i < row.length; i++) {
+			for (int j = 0; j < column.length; j++) {
+				if (matrix[i][j] == 0) {
+					// mark
+					row[i] = 1;
+					column[j] = 1;
+				}
+			}
+		}
+		for (int i = 0; i < row.length; i++) {
+			for (int j = 0; j < column.length; j++) {
+				if (row[i] == 1 || column[j] == 1) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+	}
+}
