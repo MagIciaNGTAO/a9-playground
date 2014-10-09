b4f8bf5ceb8021608c9a3537f678bb1bba189d3b
craigmingtaozhang <mail2mingtao@gmail.com>
2014-10-08 23:15:15 -0400

    sum

--- src/main/java/org/mingtaoz/leetcode/puzzle/Sum.java
+++ src/main/java/org/mingtaoz/leetcode/puzzle/Sum.java
@@ -1,7 +1,6 @@
 package org.mingtaoz.leetcode.puzzle;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -28,11 +27,16 @@ public class Sum {
 
 	public List<List<Integer>> threeSum(int[] num) {
 		List<List<Integer>> ret = new LinkedList<>();
+		if (num.length == 0) {
+			return ret;
+		}
 		Set<String> dup = new HashSet<>();
 		// 1. sort
 		Arrays.sort(num);
-		// 2. search & prunning
-		int left = 0, right = num.length - 1;
+		// 2. remove duplicate
+		int right = removeDuplicate(num);
+		// 3. search & prunning
+		int left = 0;
 		for (; left < right - 1 && num[left] <= 0; left++) {
 			for (int j = right; j > left + 1; j--) {
 				for (int i = j - 1; i > left & num[left] + num[i] + num[j] >= 0; i--) {
@@ -51,8 +55,57 @@ public class Sum {
 		return ret;
 	}
 
+	public int removeDuplicate(int[] num) {
+		int total = 0, prev = num[total];
+		int count = 0;
+		for (int i = 1; i < num.length; i++) {
+			if (prev != num[i]) {
+				num[++total] = num[i];
+				prev = num[i];
+				count = 0;
+			} else {
+				if (count == 0) {
+					count++;
+					num[++total] = num[i];
+				} else if (num[i] == 0 && count < 2) {
+					// TODO: missed three 0 case
+					count++;
+					num[++total] = num[i];
+				} else {
+					count++;
+				}
+			}
+		}
+		return total;
+	}
+
 	public int threeSumClosest(int[] num, int target) {
-		return 0;
+		int ret = 0;
+		Arrays.sort(num);
+		int right = removeDuplicate(num);
+		
+		
+		
+		return ret;
+	}
+
+	// 2222 333 -> 222 333
+	public int removeTrilicate(int[] num) {
+		int total = 0, prev = num[total];
+		int count = 0;
+		for (int i = 1; i < num.length; i++) {
+			if (prev != num[i]) {
+				num[++total] = num[i];
+				prev = num[i];
+				count = 0;
+			} else {
+				if (count < 2) {
+					count++;
+					num[++total] = num[i];
+				}
+			}
+		}
+		return total;
 	}
 
 	public List<List<Integer>> fourSum(int[] num, int target) {
