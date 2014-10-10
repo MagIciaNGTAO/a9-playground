420c48161bda73f93ea0bc7993519a306d744dab
craigmingtaozhang <mail2mingtao@gmail.com>
2014-10-09 23:17:49 -0400

    any sum.

--- src/main/java/org/mingtaoz/leetcode/puzzle/Sum.java
+++ src/main/java/org/mingtaoz/leetcode/puzzle/Sum.java
@@ -80,12 +80,23 @@ public class Sum {
 	}
 
 	public int threeSumClosest(int[] num, int target) {
-		int ret = 0;
+		int ret = 0, dist = Integer.MAX_VALUE;
 		Arrays.sort(num);
-		int right = removeDuplicate(num);
-		
-		
-		
+		int right = removeTrilicate(num);
+		int left = 0;
+		for (; left < right - 1; left++) {
+			for (int j = right; j > left + 1; j--) {
+				for (int i = j - 1; i > left; i--) {
+					if (num[left] + num[i] + num[j] - target == 0) {
+						return target;
+					}
+					if (Math.abs(num[left] + num[i] + num[j] - target) < dist) {
+						dist = Math.abs(num[left] + num[i] + num[j] - target);
+						ret = num[left] + num[i] + num[j];
+					}
+				}
+			}
+		}
 		return ret;
 	}
 
@@ -108,9 +119,92 @@ public class Sum {
 		return total;
 	}
 
+	class Wrapper {
+
+		public List<Integer> list;
+
+		public Wrapper(List<Integer> list) {
+			this.list = list;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o instanceof Wrapper) {
+				Wrapper that = (Wrapper) o;
+				return list.toString().equals(that.list.toString());
+			}
+			return false;
+		}
+
+		@Override
+		public int hashCode() {
+			return list.get(0) - list.get(1);
+		}
+	}
+
 	public List<List<Integer>> fourSum(int[] num, int target) {
 		List<List<Integer>> ret = new LinkedList<>();
-
+		if (num.length == 0) {
+			return ret;
+		}
+		Set<Wrapper> dup = new HashSet<>();
+		// 1. sort
+		Arrays.sort(num);
+		// 2. remove duplicate
+		int right = removeQuaduplets(num), left = 0;
+		// 3. search & prunning
+		int quarter = target / 4 - 1;
+		int half = target / 2 - 1;
+		int em = half + quarter;
+		for (; right > left + 2; right--) {
+			if (num[right] < quarter) {
+				break;
+			}
+			for (int i = right - 1; i > left + 1; i--) {
+				int partialOut = num[right] + num[i];
+				if (partialOut < half) {
+					break;
+				}
+				for (int j = i - 1; j > left; j--) {
+					int partial = partialOut + num[j];
+					if (partial < em) {
+						break;
+					}
+					for (int k = left; k < j; k++) {
+						int temp = partial + num[k];
+						if (temp == target) {
+							List<Integer> list = Arrays.asList(num[k], num[j],
+									num[i], num[right]);
+							Wrapper w = new Wrapper(list);
+							if (!dup.contains(w)) {
+								dup.add(w);
+								ret.add(list);
+							}
+						} else if (temp > target) {
+							break;
+						}
+					}
+				}
+			}
+		}
 		return ret;
+	}
+
+	public int removeQuaduplets(int[] num) {
+		int total = 0, prev = num[total];
+		int count = 0;
+		for (int i = 1; i < num.length; i++) {
+			if (prev != num[i]) {
+				num[++total] = num[i];
+				prev = num[i];
+				count = 0;
+			} else {
+				if (count < 3) {
+					count++;
+					num[++total] = num[i];
+				}
+			}
+		}
+		return total;
 	}
 }
