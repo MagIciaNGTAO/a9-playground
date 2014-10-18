31f52186c5b305b25b6b3ff71c3ccf239a561fd4
craigmingtaozhang <mail2mingtao@gmail.com>
2014-10-18 18:55:15 -0400

    4 left ...

---
+++ src/main/java/org/mingtaoz/leetcode/puzzle/StringToInteger.java
@@ -0,0 +1,9 @@
+package org.mingtaoz.leetcode.puzzle;
+
+public class StringToInteger {
+
+    public int atoi(String str) {
+        return 0;
+    }
+
+}
--- src/main/java/org/mingtaoz/leetcode/string/Anagrams.java
+++ src/main/java/org/mingtaoz/leetcode/string/Anagrams.java
@@ -1,16 +1,42 @@
 package org.mingtaoz.leetcode.string;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Anagrams {
-	// TODO encoding using 1-26?
-	// there could be multiple groups: bad question
-	
-	public List<String> anagrams(String[] strs) {
-		List<String> ret = new LinkedList<>();
-		
-		return ret;
-	}
 
+    private Map<String, List<String>> group = new HashMap<>();
+
+    public List<String> anagrams(String[] strs) {
+        for (int i = 0; i < strs.length; i++) {
+            int[] countPrime = new int[26];
+            for (char c : strs[i].toCharArray()) {
+                countPrime[c - 'a']++;
+            }
+            StringBuilder s = new StringBuilder();
+            for (int j = 0; j < 26; j++) {
+                s.append(countPrime[j]);
+            }
+            String t = s.toString();
+            if (group.containsKey(t)) {
+                List<String> l = group.get(t);
+                l.add(strs[i]);
+            } else {
+                List<String> l = new LinkedList<>();
+                l.add(strs[i]);
+                group.put(s.toString(), l);
+            }
+        }
+        List<String> ret = new LinkedList<>();
+        for (Entry<String, List<String>> e : group.entrySet()) {
+            List<String> l = e.getValue();
+            if (l.size() > 1) {
+                ret.addAll(l);
+            }
+        }
+        return ret;
+    }
 }
