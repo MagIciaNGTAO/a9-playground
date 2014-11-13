0ff443371d2ae1a5525875386938e30515218e85
craigmingtaozhang <mail2mingtao@gmail.com>
2014-11-12 21:54:41 -0500

    simplified ...

--- src/main/java/org/mingtaoz/leetcode/string/SimplifyPath.java
+++ src/main/java/org/mingtaoz/leetcode/string/SimplifyPath.java
@@ -11,77 +11,39 @@ import java.util.Stack;
  * 
  */
 public class SimplifyPath {
-    // TODO DRY ... the problem is kinda nasty
+
+    // the core is recognize / to / is boundary to boundary
+    // and .ssh can be file name ...
     public String simplifyPath(String path) {
-        Stack<String> stack = new Stack<>();
-        char[] characters = path.toCharArray();
-        int dotCount = 0;
-        for (int i = 0; i < characters.length; i++) {
-            if (characters[i] == '/') {
-                // case /
-                if (dotCount == 1) {
-                    stack.pop();
-                } else if (dotCount == 2) {
-                    while (!stack.isEmpty() && !stack.peek().equals("/")) {
-                        stack.pop();
+        Stack<String> folders = new Stack<>();
+        while (path.length() > 0) {
+            if (path.startsWith("/")) {
+                while (path.length() > 0 && path.startsWith("/")) {
+                    path = path.substring(1);
+                }
+                int i = 0;
+                while (i < path.length() && '/' != path.charAt(i)) {
+                    i++;
+                }
+                String temp = path.substring(0, i);
+                path = path.substring(i);
+                if ("..".equals(temp)) {
+                    if (!folders.isEmpty()) {
+                        folders.pop();
                     }
-                    if (!stack.isEmpty() && stack.size() > 1) {
-                        // pop / if exist
-                        stack.pop();
-                        // further go to next /
-                        while (!stack.isEmpty() && !stack.peek().equals("/")) {
-                            stack.pop();
-                        }
-                    }
-                } else if (!stack.isEmpty() && stack.peek().equals("/")) {
+                } else if (".".equals(temp) || temp.length() == 0) {
                     // noop
                 } else {
-                    stack.push("/");
+                    folders.push(temp);
                 }
-                dotCount = 0;
-            } else if (characters[i] == '.') {
-                dotCount++;
-                stack.push(".");
-            } else {
-                // case [a-zA-Z]
-                stack.push(characters[i] + "");
-                dotCount = 0;
             }
         }
         StringBuilder ret = new StringBuilder();
-        if (stack.size() > 1 && stack.peek().equals("/")) {
-            stack.pop();
+        while (!folders.isEmpty()) {
+            ret.insert(0, "/" + folders.pop());
         }
-        if (dotCount < 3) {
-            if (dotCount == 2) {
-                while (!stack.isEmpty() && !stack.peek().equals("/")) {
-                    stack.pop();
-                }
-                if (!stack.isEmpty() && stack.size() > 1) {
-                    // pop / if exist
-                    stack.pop();
-                    // further go to next /
-                    while (!stack.isEmpty() && !stack.peek().equals("/")) {
-                        stack.pop();
-                    }
-                    if (!stack.isEmpty() && stack.size() > 1) {
-                        stack.pop();
-                    }
-                }
-            } else {
-                while (dotCount-- > 0) {
-                    stack.pop();
-                }
-                if (!stack.isEmpty() && stack.peek().equals("/")) {
-                    stack.pop();
-                }
-            }
-        }
-        if (stack.isEmpty()) {
-            stack.push("/");
-        }
-        while (!stack.isEmpty()) {
-            ret.insert(0, stack.pop());
+        if (ret.length() == 0) {
+            ret.append("/");
         }
         return ret.toString();
     }
